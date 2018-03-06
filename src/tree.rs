acb9b9ba7023206181e1c847c53d04d8dccf4d6d
Alex Nichol <unixpickle@gmail.com>
2018-03-06 11:52:32 -0800

    tree tests pass

--- src/tree.rs
+++ src/tree.rs
@@ -1,7 +1,7 @@
 use std::cmp::PartialOrd;
 use std::ops::Index;
 
-#[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Clone, PartialEq, Eq, Debug)]
 /// A decision tree that is stored on the heap.
 pub struct Tree {
     feature: usize,
@@ -9,7 +9,7 @@ pub struct Tree {
     branch: Option<Box<Branch>>
 }
 
-#[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Clone, PartialEq, Eq, Debug)]
 pub struct Branch {
     left: Tree,
     right: Tree
@@ -18,15 +18,20 @@ pub struct Branch {
 impl Tree {
     /// Construct the "zero" tree of a given depth.
     pub fn new(depth: u8) -> Tree {
+        Tree::last(depth, 0, 0)
+    }
+
+    /// Construct the maximum tree before wrapping occurs.
+    pub fn last(depth: u8, feature_max: usize, threshold_max: u8) -> Tree {
         if depth == 0 {
-            Tree{feature: 0, threshold: 0, branch: None}
+            Tree{feature: feature_max, threshold: threshold_max, branch: None}
         } else {
             Tree{
-                feature: 0,
-                threshold: 0,
+                feature: feature_max,
+                threshold: threshold_max,
                 branch: Some(Box::new(Branch{
-                    left: Tree::new(depth - 1),
-                    right: Tree::new(depth - 1)
+                    left: Tree::last(depth - 1, feature_max, threshold_max),
+                    right: Tree::last(depth - 1, feature_max, threshold_max)
                 }))
             }
         }
@@ -74,6 +79,98 @@ impl Tree {
     }
 }
 
-fn modular_add(start: usize, add: usize, limit: usize) -> (usize, usize) {
-    ((start + add) % limit, (start + add) / limit)
+fn modular_add(start: usize, add: usize, max: usize) -> (usize, usize) {
+    ((start + add) % (max + 1), (start + add) / (max + 1))
+}
+
+#[test]
+fn tree_add() {
+    let t1 = Tree::new(2);
+
+    // Leftmost leaf becomes (feature=1, thresh=2)
+    let (t2, t2_carry) = t1.add(5, 1, 2);
+    assert_eq!(t2_carry, 0);
+    assert_eq!(t2, Tree{
+        feature: 0,
+        threshold: 0,
+        branch: Some(Box::new(Branch{
+            left: Tree{
+                feature: 0,
+                threshold: 0,
+                branch: Some(Box::new(Branch{
+                    left: Tree{
+                        feature: 1,
+                        threshold: 2,
+                        branch: None
+                    },
+                    right: Tree::new(0)
+                }))
+            },
+            right: Tree::new(1)
+        }))
+    });
+
+    // Leftmost leaf wraps and becomes (feature=0, thresh=2).
+    // Second to leftmost leaf becomes (feature=0, thresh=1).
+    let (t3, t3_carry) = t2.add(3, 1, 2);
+    assert_eq!(t3_carry, 0);
+    assert_eq!(t3, Tree{
+        feature: 0,
+        threshold: 0,
+        branch: Some(Box::new(Branch{
+            left: Tree{
+                feature: 0,
+                threshold: 0,
+                branch: Some(Box::new(Branch{
+                    left: Tree{
+                        feature: 0,
+                        threshold: 2,
+                        branch: None
+                    },
+                    right: Tree{
+                        feature: 0,
+                        threshold: 1,
+                        branch: None
+                    }
+                }))
+            },
+            right: Tree::new(1)
+        }))
+    });
+
+    // Leaf nodes have 6 combos, depth 1 has 6^3 combos,
+    // depth 2 has (6^3)^2*6 = 279936 combos.
+    let (t4, t4_carry) = t3.add(279935, 1, 2);
+    assert_eq!(t4_carry, 1);
+    assert_eq!(t4, Tree{
+        feature: 0,
+        threshold: 0,
+        branch: Some(Box::new(Branch{
+            left: Tree{
+                feature: 0,
+                threshold: 0,
+                branch: Some(Box::new(Branch{
+                    left: Tree{
+                        feature: 0,
+                        threshold: 1,
+                        branch: None
+                    },
+                    right: Tree{
+                        feature: 0,
+                        threshold: 1,
+                        branch: None
+                    }
+                }))
+            },
+            right: Tree::new(1)
+        }))
+    });
+
+    let (t5, t5_carry) = t3.add(279936 - 8, 1, 2);
+    assert_eq!(t5_carry, 1);
+    assert_eq!(t5, Tree::new(2));
+
+    let (t6, t6_carry) = t3.add(279936 - 9, 1, 2);
+    assert_eq!(t6_carry, 0);
+    assert_eq!(t6, Tree::last(2, 1, 2));
 }
